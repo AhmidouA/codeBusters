b9c53bcd5a767f7afd9f1222ae20e27315d04ff0
HemyDay <sire.amanda@gmail.com>
2024-10-17 10:40:52 +0200

    created MAP_CENTER variable

--- client/src/Style/Main_Page.tsx
+++ client/src/Style/Main_Page.tsx
@@ -1,26 +1,25 @@
 // LIBRARIES
-import react from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import react, {useState} from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
 // STYLE
 
 
 const Main_Page = () => {
+  const [MAP_CENTER, setMAP_CENTER] = useState<[number, number]>([43.62505, 3.862038]);
 
-  return(
+  return (
     <MapContainer 
-        center={[43.62505, 3.862038]} 
-        style={{height: '100vh', width:'100vw'}} 
-        zoom={15} 
-        scrollWheelZoom={true}
-      >
+      center={MAP_CENTER} 
+      style={{ height: '100vh', width: '100vw' }} 
+      zoom={15} 
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
     </MapContainer>
-
-  )
-
-}
+  );
+};
 
 export default Main_Page;
